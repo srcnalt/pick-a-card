e16052c20d42e2d2ef82a9d0c11c6272139b73cb
Sarge <sercan.altundas@gmail.com>
2018-08-03 19:33:20 +0300

    [test] test case for pick method with parmeter top:true

--- tests/Deck.test.js
+++ tests/Deck.test.js
@@ -34,4 +34,13 @@ describe('Pick', () => {
     expect(cards.length).toBe(3);
     expect(deck.cards.length).toBe(49);
   });
+
+  test('with parameter top:true extracts 1 cards from the top of the deck', () => {
+    const deck = new Deck();
+    const cards = deck.pick({position: 'top'});
+    expect(cards.length).toBe(1);
+    expect(deck.cards.length).toBe(51);
+    expect(cards[0].face).toBe("ace");
+  });
+
 });
