54bed5e0ad779f05b9fd4c211de88999937f732f
Sarge <sercan.altundas@gmail.com>
2018-08-05 10:55:34 +0300

    [feature] Test for Card created, test case for toString of Card added

---
+++ tests/Card.test.js
@@ -0,0 +1,11 @@
+const Deck = require('../Deck');
+
+describe('Card', () => {
+  test('should have a string definition', () => {
+    const deck = new Deck();
+    const card = deck.cards[0];
+    const def = card.toString();
+
+    expect(def).toBe("ace of clubs with value of 1";
+  });
+});
